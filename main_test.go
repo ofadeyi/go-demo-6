e1c23f8eaae40d0a53ab7c1d8fd88472f3885cdb
Oladapo Fadeyi <ofadeyi@gmail.com>
2019-08-01 11:07:47 +0100

    Testing (failing unit test)

--- main_test.go
+++ main_test.go
@@ -88,7 +88,7 @@ func (s *MainTestSuite) Test_HelloServer_WritesHelloWorld() {
 
 	HelloServer(w, req)
 
-	w.AssertCalled(s.T(), "Write", []byte("hello, VERSIONING!\n"))
+	w.AssertCalled(s.T(), "Write", []byte("hello, Pair Programing is good!\n"))
 }
 
 func (s *MainTestSuite) Test_HelloServer_Waits_WhenDelayIsPresent() {
